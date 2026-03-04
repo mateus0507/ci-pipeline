8bf4ae003fcfe6078f1ab0ac262814979dd345d3
Mateus <mat3us.rezende@gmail.com>
2026-03-04 10:51:02 -0300

    comentado

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
+#Apenas para testar se sobe com ou sem erros 
+
+# Função simples para somar dois números
 def soma(a: int, b: int) -> int:
+
+    # Retorna o resultado da soma
     return a + b
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
+# Importa a função soma que está no arquivo app.py
 from app import soma
 
+
+# Função de teste que o pytest vai executar automaticamente
 def test_soma():
+
+    # Verifica se 2 + 2 é igual a 4
     assert soma(2, 2) == 4
