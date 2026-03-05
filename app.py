79e94edbf9d666ee4fc467ecde9ab29f6fb4d566
Mateus <mat3us.rezende@gmail.com>
2026-03-05 10:42:33 -0300

    Add workflow to require PR branch up to date with main

--- app.py
+++
@@ -1,9 +0,0 @@
-#Apenas para testar se sobe com ou sem erros 
-
-# Função simples para somar dois números
-def soma(a: int, b: int) -> int:
-
-    # Retorna o resultado da soma
-    return a + b
-
-assert soma(2,2) == 5
